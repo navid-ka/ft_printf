00d25dbaa4b51800a9b1d7d2699f58edf54d730b
Tu Nombre <you@example.com>
2023-05-21 21:03:17 +0200

    primeras pruebas printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,19 +6,49 @@
 /*   By: bifrost <nkeyani-@student.42barcelona.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 16:42:54 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/05/19 15:20:29 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/05/21 21:02:42 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_conversion(char arg, va_list args); //cspdiuxX
+//cspdiuxX
+int ft_conversion(char *s, va_list args)
+{
+	int	check;
+
+	check = 0;
+	while (*s++)
+	{
+		if (s == "%c")
+			check = ft_putchar_fd(va_arg(args, int), 1);
+		else if (s == "%s")
+			check = ft_putstr_fd(va_arg(args, int), 1);
+	
+	}
+	return (check);
+}
 
 int	ft_printf(char const *s, ...)
 {
-    va_list args;
-    va_start(args, s);
-	
+	va_list	args;
+	int		print;
+
+	va_start(args, s);
+	print = ft_conversion(s, args);
 	va_end(args);
+	return (print);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	char a;
+
+	a = 'a';
+
+	ft_printf("%c", a);
+
 	return (0);
+
 }
