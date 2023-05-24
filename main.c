96a4e8dbaa0a399ac5d671cbccbc294782058241
bifrost <nkeyani-@student.42barcelona.com>
2023-05-24 12:23:10 +0200

    tests done

--- main.c
+++ main.c
@@ -6,15 +6,19 @@ int	main(void)
 	char a;
 	char s[] = "Hola mundo";
 	int x;
+	unsigned long h;
 
 	x = 243242312;
 	a = 'a';
 	ft_printf("%c Hola \n", a);
-	ft_printf("flag sc %s %c \n", s, a);
-	printf("flag PUUU %p \n", &x);
-	ft_printf("flag PUUU %d \n", -10);
-	printf("flag PUUU %d \n", -10);
-	printf("flag PUUU %d \n", -10);
+	ft_printf("flag ftsc %s %c \n", s, a);
+	printf("flag pP %p \n", &x);
+	ft_printf("flag ftD %d \n", -10);
+	printf("flag Df %d \n", -10);
+	ft_printf("flag ftx %x \n", &h);
+	printf("flag px %x \n", &h);
+	ft_printf("flag ftX %X \n", &h);
+	printf("flag ftX %X \n", &h);
 
 
 /*printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% 
